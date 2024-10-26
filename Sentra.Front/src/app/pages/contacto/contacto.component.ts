3899ce1d5ede9553a250a45008ce031a5de2737b
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2024-10-26 03:42:53 -0300

    agrega home y contacto

--- Sentra.Front/src/app/pages/contacto/contacto.component.ts
+++ Sentra.Front/src/app/pages/contacto/contacto.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-contacto',
+  selector: "app-contacto",
   standalone: true,
   imports: [],
-  templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  templateUrl: "./contacto.component.html",
+  styleUrl: "./contacto.component.css",
 })
-export class ContactoComponent {
-
-}
+export class ContactoComponent {}
